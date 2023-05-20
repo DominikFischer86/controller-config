d70a6490b3a94eb19935f23621e7f6786512f890
Dominik <53489780+DominikFischer86@users.noreply.github.com>
2023-05-20 03:48:28 +0200

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import pluginChecker from 'vite-plugin-checker';
-import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), pluginChecker({ typescript: true })],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+  plugins: [react()],
+  base: "./",
+  build: {
+    assetsDir: "assets", // Set the output directory for compiled assets
+    rollupOptions: {
+      output: {
+        assetFileNames: "assets/[name].[ext]", // Configure the asset file names
+      },
     },
   },
 });
